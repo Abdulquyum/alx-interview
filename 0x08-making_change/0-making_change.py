5a4038fc175679850d4ac2c86553b91baca50e28
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-20 01:14:51 +0100

    Coin changer Function

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Determine the fewest number of coins needed to meet a given amount total """
+
+def makeChange(coins, total):
+    """ Change Coin Function  """
+    def makeChange(coins, total):
+    """ Determine the fewest number of coins needed to meet a given total """
+    if total <= 0:
+        return 0
+
+    # Initialize a list to store the minimum coins needed for each amount
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0  # No coins needed to make 0 total
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
