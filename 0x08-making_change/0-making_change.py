4b87ece07b9667a8293892155c11b864900000d4
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-20 05:43:43 +0100

    Coin changer Function

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,13 +10,13 @@ def makeChange(coins: int, total: int) -> int:
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    minimum_coins = [float('inf')] * (total + 1)
+    minimum_coins[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+            minimum_coins[i] = min(minimum_coins[i], minimum_coins[i - coin] + 1)
 
-    if dp[total] != float('inf'):
-        return (dp[total]) 
+    if minimum_coins[total] != float('inf'):
+        return (minimum_coins[total]) 
     return (-1)
