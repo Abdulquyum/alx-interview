6004bccea103927b2e5a7f0a10897e1c31833e5a
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-27 05:16:37 +0100

    calculate the perimeter of the provided island rows and columns using the knowledge of list comprehension

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,15 +12,12 @@ def island_perimeter(grid: list[list[int]]) -> int:
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                # Start with 4 for each land cell
                 perimeter += 4
                 
-                # Check the left neighbor
                 if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2  # One side is shared, so subtract 2
+                    perimeter -= 2
                 
-                # Check the top neighbor
                 if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2  # One side is shared, so subtract 2
+                    perimeter -= 2
 
     return perimeter
